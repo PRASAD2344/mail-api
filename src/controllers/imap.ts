6e49ceb45b170726f3921d33d4287105969d137f
Prasad Medarametla <medarametla.l.v.prasad@gmail.com>
2021-05-26 12:31:50 +0530

    Password being encrypted while sending over network.

--- src/controllers/imap.ts
+++ src/controllers/imap.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import {ImapFlow} from 'imapflow'
 import {simpleParser} from 'mailparser';
+import * as CryptoJS from 'crypto-js';
 
 const imapQueryOptionsWhenGetOne = {
     envelope: true,
@@ -17,6 +18,7 @@ const defaultPath = 'INBOX';
 
 export const listEmails = async (req: Request, res: Response) => {
     const imapClientConfiguration = req.body as IMAPClientConfiguration;
+    imapClientConfiguration.auth.pass = decryptData(imapClientConfiguration.auth.pass);
     const path = req.query.path ? req.query.path : defaultPath;
     const client = new ImapFlow(imapClientConfiguration);
     await client.connect();
@@ -36,8 +38,9 @@ export const listEmails = async (req: Request, res: Response) => {
 
 export const getEmail = async (req: Request, res: Response) => {
     const uid = req.params.uid as string;
-    const path = req.query.path ? req.query.path : defaultPath;
     const imapClientConfiguration = req.body as IMAPClientConfiguration;
+    imapClientConfiguration.auth.pass = decryptData(imapClientConfiguration.auth.pass);
+    const path = req.query.path ? req.query.path : defaultPath;
     const client = new ImapFlow(imapClientConfiguration);
     await client.connect();
     const lock = await client.getMailboxLock(path);
@@ -64,6 +67,7 @@ export const getEmail = async (req: Request, res: Response) => {
 
 export const listMailBoxes = async (req: Request, res: Response) => {
     const imapClientConfiguration = req.body as IMAPClientConfiguration;
+    imapClientConfiguration.auth.pass = decryptData(imapClientConfiguration.auth.pass);
     const client = new ImapFlow(imapClientConfiguration);
     await client.connect();
     try {
@@ -79,4 +83,13 @@ const toString = (data: any) => {
     return JSON.stringify(data, (key, value) =>
         typeof value === "bigint" ? value.toString() : value
     );
+}
+
+const decryptData = (data: string) => {
+    const encryptionKey = "j7WM}xMj9w|hkdG";
+    const bytes = CryptoJS.AES.decrypt(data, encryptionKey);
+    if (bytes.toString()) {
+        return bytes.toString(CryptoJS.enc.Utf8);
+    }
+    return data;
 }
